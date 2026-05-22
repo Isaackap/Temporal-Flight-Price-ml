9770e5f80fbc90e45548e321b92691cf842cf9b8
Isaac <ikapeel@gmail.com>
2026-05-21 20:04:36 -0500

    Updated functions to handle selected dates from dashboard and config dates from config file

--- scrape_flights.py
+++ scrape_flights.py
@@ -37,24 +37,6 @@ def sendEmail():
     # Clear file once email is sent
     open(os.path.join(RUNTIME_DIR, "flight_alert.txt"), "w").close()
 
-# API querystring that imports the parameters set in the config.py file
-# -------- IMPORTANT ---------
-# If you left any of the optional parameters in config.py blank such as "child_ages", or if it has a value but you want to omit it
-# then you must comment it out of the querystring dict below.
-querystring = {
-    "fromId":config.SOURCE_AIRPORT_CODE,                           # REQUIRED PARAMETER
-    "toId":config.DESTINATION_AIRPORT_CODE,                        # REQUIRED PARAMETER
-    "departDate":config.DEPARTURE_DATE,                            # REQUIRED PARAMETER
-    "returnDate":config.RETURN_DATE,                               # OPTIONAL
-    #"stops":config.STOPS,                                          # OPTIONAL
-    "pageNo":config.PAGE_NUMBER,                                   # OPTIONAL
-    "adults":config.ADULTS,                                        # OPTIONAL
-    #"children":config.CHILDREN,                                    # OPTIONAL
-    "sort":config.SORT_ORDER,                                      # OPTIONAL
-    "cabinClass":config.CABIN_CLASS,                               # OPTIONAL
-    "currency_code":config.CURRENCY_CODE,                          # OPTIONAL
-}
-
 
 # Mock data used to test features before implementing actual API calls with a key
 # Response is the JSON given as example response on rapidapi
@@ -65,7 +47,7 @@ def mockAPI():
     return data["data"]["flightOffers"]
 
 # Actual API call, uses BOOKING.COM from Rapidapi.com
-def callAPI():
+def callAPI(querystring):
     response = requests.get(config.SEARCH_FLIGHTS_API_URL, headers=config.HEADERS, params=querystring)
 
     data = response.json()
@@ -108,7 +90,7 @@ def flightAlert(data: dict):
                    
 
 # Search through the response data for flight info and prices
-def searchFlightOffers(data: dict):
+def searchFlightOffers(data: dict, departure_date: str, return_date: str):
     day_of_week = datetime.now().strftime('%A')
     with open(os.path.join(RUNTIME_DIR, "flight_data.txt"), "w+") as file:
         for flightOffers in data:
@@ -120,7 +102,7 @@ def searchFlightOffers(data: dict):
                 f"{getTimeOfDay(segments["departureTime"])},{(len(segments["legs"])) - 1},{getFlightDuration(segments["totalTime"])},{segments["arrivalAirport"]["cityName"]},"
                 f"{segments["arrivalAirport"]["code"]},{getTimeOfDay(segments["arrivalTime"])},{legs["cabinClass"]},{flightOffers["tripType"]},"
                 f"{flightOffers["priceBreakdown"]["total"]["currencyCode"]},{flightOffers["priceBreakdown"]["total"]["units"]},{day_of_week},"
-                f"{datetime.today().strftime('%Y/%m/%d')},{config.DEPARTURE_DATE},{config.RETURN_DATE}\n")
+                f"{datetime.today().strftime('%Y/%m/%d')},{departure_date},{return_date}\n")
             
             # Saves specified data in a dictionary to then be formatted in the email body if it meets the PRICE_THRESHOLD
             if flightOffers["priceBreakdown"]["total"]["units"] <= config.PRICE_THRESHOLD:
@@ -135,20 +117,41 @@ def searchFlightOffers(data: dict):
                     "itinerary_type":flightOffers["tripType"],
                     "total_cost":flightOffers["priceBreakdown"]["total"]["units"],
                     "currency":flightOffers["priceBreakdown"]["total"]["currencyCode"],
-                    "departure_date":config.DEPARTURE_DATE,
-                    "return_date":config.RETURN_DATE
+                    "departure_date":departure_date,
+                    "return_date":return_date
                 }
                 flightAlert(alert_data)
 
-def main():
+def main(departure_date: str = config.DEPARTURE_DATE, return_date: str = config.RETURN_DATE):
+    departure_date = departure_date
+    return_date = return_date
+
+    # API querystring that imports the parameters set in the config.py file
+    # -------- IMPORTANT ---------
+    # If you left any of the optional parameters in config.py blank such as "child_ages", or if it has a value but you want to omit it
+    # then you must comment it out of the querystring dict below.
+    querystring = {
+        "fromId":config.SOURCE_AIRPORT_CODE,                           # REQUIRED PARAMETER
+        "toId":config.DESTINATION_AIRPORT_CODE,                        # REQUIRED PARAMETER
+        "departDate":departure_date,                                   # REQUIRED PARAMETER
+        "returnDate":return_date,                                      # OPTIONAL
+        #"stops":config.STOPS,                                          # OPTIONAL
+        "pageNo":config.PAGE_NUMBER,                                   # OPTIONAL
+        "adults":config.ADULTS,                                        # OPTIONAL
+        #"children":config.CHILDREN,                                    # OPTIONAL
+        "sort":config.SORT_ORDER,                                      # OPTIONAL
+        "cabinClass":config.CABIN_CLASS,                               # OPTIONAL
+        "currency_code":config.CURRENCY_CODE,                          # OPTIONAL
+    }
+
     #mock_data = mockAPI()
     #searchFlightOffers(mock_data)
     # Comment out both lines if not using AWS Secrets Manager
     # getCredentials("Token", "token.json")
     # getCredentials("Credentials", "credentials.json")
-    response, data = callAPI()
+    response, data = callAPI(querystring)
     if response.ok:
-        searchFlightOffers(data)
+        searchFlightOffers(data, departure_date, return_date)
         # Comment out if you don't want Email alerts
         sendEmail()
         # Comment out if you don't want data exported to Google Sheets
